9fef01e5a486504a38be2b99a14e6fa71b778abb
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-12-07 19:02:56 +0500

    Cubic Feet

---
+++ Cubic feet.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float g,cf;
+	cout<<"Enter The Volume In Gallons\n";
+	cin>>g;
+	cf=g/7.481;
+	cout<<"The Volume In Cubic Feet Is\n"<<cf;
+	return  0;
+}
