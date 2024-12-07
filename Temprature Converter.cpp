0f8cc5ff4f356fda0fa63f21e726e895a6a579dc
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-12-07 19:04:02 +0500

    Temprature Converter

---
+++ Temprature Converter.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float Dc,Df;
+	cout<<"Enter Temprature In Degree Celcius\n";
+	cin>>Dc;
+	Df=(Dc*9/5)+32;
+	cout<<"The Temprature In Degree Farenheit Is\n"<<Df;
+    return 0;
+}
