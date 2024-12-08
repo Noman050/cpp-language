f78d9adc1c11a676739373344fe55022505868cb
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-12-08 23:08:15 +0500

    programme.cpp

---
+++ programme.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{int a,b;
+    cout<<"Enter The First Number\n";cin>>a;
+	cout<<"Enter The 2nd Number\n";cin>>b;
+	cout<<"\nAddition is="<<a+b<<"\nSubtration is="<<a-b<<"\nMultiplication is="<<a*b<<"\nDivision is="<<a/b<<"\nModulus is="<<a%b;cout<<"\n\n\nBy Nomi Rana";
+	getch();
+ }                                                                                                                                                                                                                                                                                                                                                      
